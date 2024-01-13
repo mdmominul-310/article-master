0f29a00b1e53939c15acd1d5ed3b4c66009dee17
mdmominul-310 <mdmominulislam310@gmail.com>
2024-01-14 01:41:33 +0600

    prisma configure

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import { Footer } from '@/components/footer/Footer'
 import { NavigationBar } from '@/components/navigationbar/navigation-bar'
-
 import HeroSection from '@/components/hero-section/HeroSection'
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className=''>
       <NavigationBar />
       <HeroSection />
       <Footer />
-      {/* <Button onClick={()=>setTheme("light")}>Button</Button> */}
     </main>
   )
 }
