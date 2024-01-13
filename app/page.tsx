5cf65b17d3c2b4dcd216c6c157f60e8e76d9e921
mdmominul-310 <mdmominulislam310@gmail.com>
2024-01-13 23:21:11 +0600

    fix the leta

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import { ActivityLog } from '@/components/activity-log/ActivityLog'
 import { Footer } from '@/components/footer/Footer'
 import { NavigationBar } from '@/components/navigationbar/navigation-bar'
-import { Button } from '@/components/ui/button'
-import { useTheme } from 'next-themes'
-import Image from 'next/image'
-import {
-  DrawerTrigger,
-} from '@/components/ui/drawer'
+
 import HeroSection from '@/components/hero-section/HeroSection'
 
 export default function Home() {
-  // const { setTheme } = useTheme()
   return (
     <main className=''>
       <NavigationBar />
